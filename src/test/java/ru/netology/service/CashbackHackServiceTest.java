d6352482207bde8e785aa844f38dc0867b506c08
Vitkovskiy <vitkovskiy1912@mail.ru>
2025-11-22 14:58:20 +0300

    Create Tests in JUnit 4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testPositiveUnderThousand(){
+
+        int amount = 550;
+        CashbackHackService service = new CashbackHackService();
+        int expected = 450;
+        int actual =  service.remain(amount);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testPositiveMoreThousand(){
+
+        int amount = 2345;
+        CashbackHackService service = new CashbackHackService();
+        int expected = 655;
+        int actual =  service.remain(amount);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testNegative(){
+
+        int amount = -2345;
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1345;
+        int actual =  service.remain(amount);
+        Assert.assertEquals(expected,actual);
+    }
+}
